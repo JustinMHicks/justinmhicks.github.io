b59cae46a0d75352122ade0dbeb2157509f58733
Justin Hicks <justinmichaelhicks@gmail.com>
2016-08-28 13:34:26 -0400

    Bouncing Titles

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$ (document).ready(function()
+                   {
+    
+$ (.bounceTitle).hover(function()
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+                      )}
